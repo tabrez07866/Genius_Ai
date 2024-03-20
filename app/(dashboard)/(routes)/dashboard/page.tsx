c1f5df2738d08264ab5d2598054a83b04758b008
tabrez07866 <tabrezkhan100820033ss@gmail.com>
2024-03-20 15:31:51 +0530

    first commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard (protected)</p>
+      
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const LandingPage=()=>{
+    return(
+        <div>Landing Page (unprotected)
+    
+            <div>
+                <Link href="/sign-in">
+                <Button>
+                    login
+                </Button>
+                </Link>
+
+                <Link href="/sign-up">
+                <Button>
+                    Register
+                </Button>
+                </Link>
+                
+            </div>
+        </div>
+        
+    )
+}
+export default LandingPage;
